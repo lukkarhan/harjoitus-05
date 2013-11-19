f0f6384d5874bad3560126289e85e7887fc13a6b
lukkarhan <hanna.lukkarinen@hotmail.fi>
2013-11-19 14:51:45 +0200

    Harjotus

---
+++ esim10_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std; 
+void Aliohjelma(void);	// esittely (aliohjelmien nimet yleens„ isolla alkukirjaimella)
+
+int main() 
+{ 
+ Aliohjelma(); // kutsu 
+ return 0; 
+} 
+
+void Aliohjelma() // m„„rittely 
+{ 
+ cout<<"T„m„ on Aliohjelma"<<endl; 
+}
