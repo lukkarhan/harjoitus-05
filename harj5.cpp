a9dee878411fb96a667bb50fb56e54cbe0b92dbd
lukkarhan <hanna.lukkarinen@hotmail.fi>
2013-11-26 16:27:47 +0200

    Harjoitus 5 (Bingo)

---
+++ harj5.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+using namespace std; 
+int main() 
+{ 
+ long luku1, luku2;
+
+ cout<<"Anna luku.";
+ cin>>luku1;
+ cout<<"Anna toinen luku.";
+ cin>>luku2;
+
+ if (luku1 == luku2)
+	 cout<<"BINGO.";
+ else
+	cout<<"BONGO.";
+
+ return 0;
+}
